cf2cf753a2bd5cde925cab432682e5078bb0e495
ecee <you@example.com>
2023-09-30 22:17:01 +0300

    Done

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(tutorialNames) {
+  return tutorialNames.map(function(name) {
+      return toTitleCase(name);
+  });
 }
